f3aeb7b6a66fd3e303c4d04d73b36705c590f033
saguywalker <saguywalker@protonmail.com>
2023-06-04 15:00:16 +0900

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to saguywalker/monkey-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/saguywalker/monkey_clj_test.clj
@@ -0,0 +1,7 @@
+(ns saguywalker.monkey-clj-test
+  (:require [clojure.test :refer :all]
+            [saguywalker.monkey-clj :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
