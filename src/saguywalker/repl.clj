85bafa2a1bac42df974a1fbb5518626a4e778e4c
saguywalker <saguywalker@protonmail.com>
2023-06-07 23:32:46 +0900

    init repl

---
+++ src/saguywalker/repl.clj
@@ -0,0 +1,22 @@
+(ns saguywalker.repl
+  (:require [saguywalker.lexer :as lexer]
+            [saguywalker.token :as token]))
+
+(def PROMPT ">> ")
+
+(defn- loop-read-token [lexer-atom]
+  (let [tok (lexer/next-token lexer-atom)]
+    (when (not= token/EOF (:type tok))
+      (println tok)
+      (recur lexer-atom))))
+
+(defn start []
+  (println PROMPT)
+  (let [line (read-line)
+        lexer-atom (lexer/new-lexer line)]
+    (loop-read-token lexer-atom)
+    (recur)))
+
+(comment
+  (def test-lexer (lexer/new-lexer "let hello = 1337;"))
+  (loop-read-token test-lexer))
