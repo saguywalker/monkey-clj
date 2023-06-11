7bdf3a2a415906202ecce20d323222d51eeea24d
saguywalker <saguywalker@protonmail.com>
2023-06-11 14:30:16 +0900

    add tostring functions

--- src/saguywalker/ast.clj
+++ src/saguywalker/ast.clj
@@ -1,6 +1,45 @@
 (ns saguywalker.ast
   (:require [saguywalker.token :as token]))
 
+(defn let-stmt->name-string [stmt]
+  (get-in stmt [:name :token :literal]))
+
+(defn let-stmt->value-string [stmt]
+  (get-in stmt [:name :value :token :literal]))
+
+(defn- let-stmt->string [stmt]
+  (str (token/token-literal stmt)
+       " "
+       (let-stmt->name-string stmt)
+       " = "
+       (let-stmt->value-string stmt) ";"))
+
+(defn expression-stmt->return-value [stmt]
+  (get-in stmt [:return-value]))
+
+(defn- return-stmt->string [stmt]
+  (str (token/token-literal stmt)
+       " "
+       (expression-stmt->return-value stmt) ";"))
+
+(defn- expression->string [expression]
+  (str expression))
+
+(defn- expression-stmt->string [stmt]
+  (str (expression->string (:expression stmt))))
+
+(defn stmt->string [stmt]
+  (let [token-type (get-in stmt [:token :type])]
+    (cond
+      (= token-type token/LET) (let-stmt->string stmt)
+      (= token-type token/RETURN) (return-stmt->string stmt)
+      :else (expression-stmt->string stmt))))
+
+(defn program->string [program]
+  (reduce (fn [acc stmt]
+            (str acc (stmt->string stmt)))
+          ""
+          (:statements program)))
 
 (defn token-literal-from-program [program]
   (let [statements (:statements program)]
---
+++ test/saguywalker/ast_test.clj
@@ -0,0 +1,14 @@
+(ns saguywalker.ast-test
+  (:require [clojure.test :refer :all]
+            [saguywalker.ast :as ast]
+            [saguywalker.token :as token]))
+
+(deftest test-string
+  (let [program {:statements [{:token {:type token/LET
+                                       :literal "let"}
+                               :name {:token {:type token/IDENT
+                                              :literal "myVar"}
+                                      :value {:token {:type token/IDENT
+                                                      :literal "anotherVar"}
+                                              :value "anotherVar"}}}]}]
+    (is (= (ast/program->string program) "let myVar = anotherVar;"))))
